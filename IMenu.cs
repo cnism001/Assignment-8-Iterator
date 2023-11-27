1d77f449bd4cbe947fa262044d4797777d90180d
Nikita Smirnov <cnism001@edu.xamk.fi>
2023-11-27 17:19:42 +0200

    Added empty classes, fleshed out IMenu class withpreperties, constructor and interface

---
+++ IMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_8_Iterator
+{
+    
+    public interface IMenu
+    {
+        
+        IEnumerator<MenuItem> CreateIterator();
+    }
+    // Class for an individual items on any of menus.
+    public class MenuItem
+    {
+        // Properties to hold the details of a menu item.
+        public string Name { get; }
+        public string Description { get; }
+        public bool IsVegetarian { get; }
+        public double Price { get; }
+
+        // Constructor to initialize a new MenuItem.
+       
+        public MenuItem(string name, string description, bool isVegetarian, double price)
+        {
+            Name = name;
+            Description = description;
+            IsVegetarian = isVegetarian;
+            Price = price;
+        }
+    }
+}
